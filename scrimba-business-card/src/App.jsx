b33b1d60eb76bcb916eebf8bd2a571a9156ed758
ericness <ericness@users.noreply.github.com>
2024-11-23 13:19:08 +0000

    Set up social logos

--- scrimba-business-card/src/App.jsx
+++ scrimba-business-card/src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import linkedInLogo from './assets/linkedin.svg'
+import mediumLogo from './assets/medium.svg'
+import githubLogo from './assets/github.svg'
+import blueskyLogo from './assets/bluesky.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +33,21 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Header />
+      <footer>
+        <a href="https://linkedin.com/in/ericness" target="_blank">
+          <img src={linkedInLogo} className="logo" alt="LinkedIn logo" />
+        </a>
+        <a href="https://medium.com/@ericness" target="_blank">
+          <img src={mediumLogo} className="logo" alt="Medium logo" />
+        </a>
+        <a href="https://github.com/ericness" target="_blank">
+          <img src={githubLogo} className="logo" alt="GitHub logo" />
+        </a>
+        <a href="https://blueskydigital.io" target="_blank">
+          <img src={blueskyLogo} className="logo" alt="Blue Sky Digital logo" />
+        </a>
+      </footer>
     </>
   )
 }
---
+++ scrimba-business-card/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header(){
+    return (
+        <header>
+            <h1>Eric Ness</h1>
+            <h2>Principal Machine Learning Engineer</h2>
+
+        </header>
+    )
+}
+
+export default Header
