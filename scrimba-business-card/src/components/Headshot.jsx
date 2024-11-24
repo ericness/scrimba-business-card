d2e1ae5684a9b80196a2c3ef2ba7c87fe99789d8
ericness <ericness@users.noreply.github.com>
2024-11-24 20:00:20 +0000

    Move headshot image

--- scrimba-business-card/src/components/Headshot.jsx
+++ scrimba-business-card/src/components/Headshot.jsx
@@ -1,7 +1,7 @@
 function Headshot() {
     return (
         <div className="profile-photo">
-            <img src="src/assets/Eric_Ness_1756_WEBSIZE.jpg" className="headshot" alt="Eric Ness headshot" />
+            <img src="public/Eric_Ness_1756_WEBSIZE.jpg" className="headshot" alt="Eric Ness headshot" />
         </div>
     )
 }
